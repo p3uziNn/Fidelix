90bc2b73752cfeaea234709fcb552d32718fa30f
Erick-art333 <silentshot1.02.0@gmail.com>
2026-04-08 15:08:59 -0300

    titulo alterado

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Got',
+      title: 'Festa da salchicha',
       theme: ThemeData(
         // This is the theme of your application.
         //
